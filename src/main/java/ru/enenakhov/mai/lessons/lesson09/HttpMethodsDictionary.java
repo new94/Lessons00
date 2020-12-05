e8efb7027a0ed1681e1bf552e85618563c6c4dc3
Ext-E.Nenakhov <enenakhov@luxoft.com>
2020-12-05 10:51:09 +0300

    lesson 9 Rest API

--- src/main/java/ru/enenakhov/mai/lessons/lesson09/HttpMethodsDictionary.java
+++
@@ -1,6 +0,0 @@
-package ru.enenakhov.mai.lessons.lesson09;
-
-public class HttpMethodsDictionary {
-    public static final String GET = "GET";
-    public static final String POST = "POST";
-}
--- src/main/java/ru/enenakhov/mai/lessons/lesson09/RestAPI.java
+++ src/main/java/ru/enenakhov/mai/lessons/lesson09/RestAPI.java
@@ -13,26 +13,28 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import static ru.enenakhov.mai.lessons.lesson09.HttpMethodsDictionary.GET;
-
+//SOAP API
+//RESTfull API
+//Tomcat, WebSphere, Jetty, Apache, WildFly JBoss, Nginx
+//Heroku
+//Servlet
 public class RestAPI {
 
     private static final Logger logger = Logger.getLogger(RestAPI.class.getName());
+    private static final Integer PORT = 8082;
 
-    private static final Integer PORT = 8000;
+    private static final String GET = "GET";
 
     public static void main(String[] args) {
         try {
-            HttpServer webServer = HttpServer.create(new InetSocketAddress(PORT), 0);
-            HttpContext context = webServer.createContext("/api/hello", httpExchange -> {
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            HttpContext contextHello = server.createContext("/api/hello", httpExchange -> {
                 if (httpExchange.getRequestMethod().equalsIgnoreCase(GET)) {
                     String query = httpExchange.getRequestURI().getQuery();
                     Map<String, String> parameters = splitParameters(query);
 
-                    String responseText = String.format("<h1>Hello %s!!!</h1>",
-                            parameters.getOrDefault("name", "noname"));
-
-                    responseText += String.format("<body>%s</body>", parameters.toString());
+                    String responseText = String.format("<h1>Hello %s!</h1><h2>All parameters: %s</h2>",
+                            parameters.getOrDefault("name", "noname"), parameters.toString());
 
                     httpExchange.sendResponseHeaders(200, responseText.getBytes().length);
                     OutputStream os = httpExchange.getResponseBody();
@@ -40,38 +42,39 @@ public class RestAPI {
                     os.flush();
                     httpExchange.close();
                 } else {
-                    httpExchange.sendResponseHeaders(405, -1);
+                    logger.info("Запрос мы не обрабатываем: " + httpExchange.getRequestMethod());
                 }
             });
 
-            context.setAuthenticator(new BasicAuthenticator("default") {
+            server.createContext("/api/by", httpExchange -> {
+                String responseText = "<h1>BY</h1>";
+                httpExchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(responseText.getBytes());
+                os.flush();
+                httpExchange.close();
+            });
+
+            contextHello.setAuthenticator(new BasicAuthenticator("default") {
                 @Override
                 public boolean checkCredentials(String user, String password) {
                     return user.equals("evgenii") && password.equals("1234");
                 }
             });
 
-            webServer.createContext("/api/by", httpExchange -> {
-                Map<String, String> parameters = splitParameters(httpExchange.getRequestURI().getQuery());
-                String response = String.format("I'll be back %s", parameters.toString());
-                httpExchange.sendResponseHeaders(200, response.getBytes().length);
-                OutputStream os = httpExchange.getResponseBody();
-                os.write(response.getBytes());
-                os.flush();
-                httpExchange.close();
-            });
-
-            webServer.start();
-            logger.info("RestAPI server started on port: " + PORT);
+            server.start();
+            logger.info("Rest API сервис стартовал на порту " + PORT);
         } catch (IOException ex) {
-            logger.error("Ошибка при запуске веб сервера на порту: " + PORT, ex);
+            logger.error("Проблема с веб сервером на порту " + PORT, ex);
         }
+
     }
 
     public static Map<String, String> splitParameters(String query) {
         return Pattern.compile("&")
                 .splitAsStream(Optional.of(query).orElse(""))
                 .map(s -> s.split("=", 2))
-                .collect(Collectors.toMap(s -> s[0], s -> s[1]));
+                .collect(Collectors.toMap(a -> a[0], a -> a[1]));
     }
+
 }
