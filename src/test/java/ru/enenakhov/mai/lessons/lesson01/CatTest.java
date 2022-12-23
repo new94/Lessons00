ef8a3aa66b92c748fe20936b766f76fb729cf45a
Ext-E.Nenakhov <enenakhov@luxoft.com>
2022-12-24 01:39:09 +0300

    create first test

---
+++ src/test/java/ru/enenakhov/mai/lessons/lesson01/CatTest.java
@@ -0,0 +1,16 @@
+package ru.enenakhov.mai.lessons.lesson01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CatTest {
+
+    @Test
+    void build() {
+        Cat catActual = Cat.build("cat", 10.0, 10);
+        assertEquals("cat", catActual.name);
+        assertEquals(1, catActual.weight);
+    }
+
+}
