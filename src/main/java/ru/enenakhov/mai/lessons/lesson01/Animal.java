d7d1bd1c3b41ff5c4b6274ac1c14678a754f526d
Ненахов Евгений Валентинович <yvnenak2@mts.ru>
2023-09-16 11:46:50 +0300

    [MAI-0001] - Изменения для демонстрации

--- src/main/java/ru/enenakhov/mai/lessons/lesson01/Animal.java
+++ src/main/java/ru/enenakhov/mai/lessons/lesson01/Animal.java
@@ -3,5 +3,6 @@ package ru.enenakhov.mai.lessons.lesson01;
 public abstract class Animal {
     public void say() {
         System.out.println("Hello, I'm Animal!");
+        System.out.println("MyChanges");
     }
 }
